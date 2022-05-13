5aad68391d8c5b182e26c45a7001fc39c5d70d78
jagadi26 <jagadi26@gmail.com>
2022-05-13 11:25:48 +0530

    2 files modified

--- src/Components/ExpenseDate.js
+++ src/Components/ExpenseDate.js
@@ -1,9 +1,10 @@
 import React from 'react';
+
 function ExpenseDate(props) {
   const day = props.date.toLocaleString('en-US', { day: '2-digit' });
   const month = props.date.toLocaleString('en-US', { month: 'long' });
   const year = props.date.getFullYear();
-  
+
   return (
     <div>
       <div>{month}</div>
