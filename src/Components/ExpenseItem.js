eb4740483760e1030fc7b380e5405a24b11c5e5f
jagadi26 <jagadi26@gmail.com>
2022-05-13 10:36:12 +0530

    1 file modified

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,8 +1,16 @@
 import React from 'react';
 function ExpenseItem(props) {
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const year = props.date.getFullYear();
+
   return (
     <div>
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div>{props.title}</div>
       <div>${props.amount}</div>
     </div>
